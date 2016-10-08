e8c33d9e6063415e810d580e1890b9f26e7ca5bc
leiwang <wang.bonn@gmail.com>
2016-10-08 16:49:34 +0200

    rm xdebug log

--- pre-init.d/20-extensions.sh
+++ pre-init.d/20-extensions.sh
@@ -7,4 +7,3 @@ echo  "xdebug.remote_autostart=1" >> /etc/php5/conf.d/xdebug.ini
 echo  "xdebug.remote_enable=1" >> /etc/php5/conf.d/xdebug.ini
 echo  "xdebug.remote_connect_back=0" >> /etc/php5/conf.d/xdebug.ini
 echo  "xdebug.max_nesting_level=400" >> /etc/php5/conf.d/xdebug.ini
-echo  "xdebug.remote_log=/var/log/xdebug.log" >> /etc/php5/conf.d/xdebug.ini
